58a5909ce5bf8b8c6b5dd1ee519ef1fa1fe52f36
Morgan Taylor <morgantaylor.mtt@outlook.com>
2025-11-12 11:09:08 +0000

    remove config excess

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/backend/:path*',
-        destination: 'http://127.0.0.1:5000/:path*',
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true }, // required for static export if using Next images
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/backend/:path*',
+  //       destination: 'http://127.0.0.1:5000/:path*',
+  //     },
+  //   ]
+  // }
+}
